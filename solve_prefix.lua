30ceeefc112b4f42a1bb24c3fac7f8508e714d5e
thetrung <deulamco@gmail.com>
2026-03-31 06:27:57 +0700

    refine code.

--- solve_prefix.lua
+++ solve_prefix.lua
@@ -1,24 +1,28 @@
 -- Lua implementation of PHP scandir function
-function scandir(directory)
-    local i, t, popen = 0, {}, io.popen
-    local pfile = popen('ls -a "'..directory..'"')
-    for filename in pfile:lines() do
-        i = i + 1
-        t[i] = filename
-        -- print(filename)
-    end
-    pfile:close()
-    return t
+local function scandir(directory)
+	local i, t, popen = 0, {}, io.popen
+	local pfile = popen('ls -a "' .. directory .. '"')
+	if pfile == nil then
+		return
+	end
+	-- if not empty :
+	for filename in pfile:lines() do
+		i = i + 1
+		t[i] = filename
+		-- print(filename)
+	end
+	pfile:close()
+	return t
 end
 
-local files = scandir('.')
+local files = scandir(".")
 
-for _,old_name in pairs(files) do
-    local found = string.find(old_name, ".png")
-    if found then
-        -- let trim them :
-        local new_name = string.sub(old_name, 13, string.len(old_name))
-        os.rename(old_name, new_name)
-        print(old_name .. "->" .. new_name)
-    end
+for tmp, old_name in pairs(files) do
+	local found = string.find(old_name, ".png")
+	if found then
+		-- let trim them :
+		local new_name = string.sub(old_name, 13, string.len(old_name))
+		os.rename(old_name, new_name)
+		print(old_name .. "->" .. new_name)
+	end
 end
